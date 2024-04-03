a20e7cddb4915cb3f8a8212047d155d200b4d5d2
GHY66688 <995724885@qq.com>
2024-04-03 16:14:00 +0800

    finish premake5.lua and finish test(includeing rebuild sln and vcxproj.filters)

--- premake5.lua
+++ premake5.lua
@@ -7,3 +7,101 @@ workspace "MiniEngine"
 		"Release",
 		"Dist"
 	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "MiniEngine"
+	location "MiniEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"MG_PLATFORM_WINDOWS",
+			"MG_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/SandBox")
+		}
+
+	filter "configurations:Debug"
+		defines "MG_DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "MG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MG_DIST"
+		optimize "On"
+	
+
+project "SandBox"
+	location "SandBox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"MiniEngine/vendor/spdlog/include",
+		"MiniEngine/src"
+	}
+
+	links
+	{
+		"MiniEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"MG_PLATFORM_WINDOWS"
+		}
+
+
+	filter "configurations:Debug"
+		defines "MG_DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "MG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MG_DIST"
+		optimize "On"
